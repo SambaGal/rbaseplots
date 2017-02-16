794cb5874863991c5493e4fcbf3abd32a8de75e1
Maria <mfreydlin@gmail.com>
2017-02-16 17:52:18 -0500

    cleaned up code

--- plot2.R
+++ plot2.R
@@ -1,10 +1,9 @@
 library(data.table)
 library(tidyr)
-#library(chron)
+
 power <- fread("household_power_consumption.txt")
 power_sub <- subset(power, Date == "1/2/2007" | Date == "2/2/2007")
-#power_sub$Date <- as.Date(power_sub$Date, format = "%d/%m/%Y")
-#power_sub$Time <- chron(times = power_sub$Time)
+
 power_sub <- as.data.frame(power_sub)
 power_sub <- unite(power_sub, Date_Time, Date, Time, sep = " ")
 power_sub$Global_active_power <- as.numeric(power_sub$Global_active_power)
--- plot3.R
+++ plot3.R
@@ -12,5 +12,5 @@ plot(power_sub$Date_Time, power_sub$Sub_metering_1, type = 'l', xlab = '', ylab 
 lines(power_sub$Date_Time, power_sub$Sub_metering_2, col = 'red')
 lines(power_sub$Date_Time, power_sub$Sub_metering_3, col = 'blue')
 legend("topright",legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
-#dev.copy(png, file = "plot3.png")
+
 dev.off()
